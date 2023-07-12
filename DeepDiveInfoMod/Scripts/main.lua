6a0794a8179c03e4140d8b610eab1fa81e5d36cb
许杰友 Jieyou Xu (Joe) <39484203+jieyouxu@users.noreply.github.com>
2023-07-12 13:19:05 +0800

    Add WIP

--- DeepDiveInfoMod/Scripts/main.lua
+++ DeepDiveInfoMod/Scripts/main.lua
@@ -10,47 +10,67 @@ local GetPlayerController = UEHelpers.GetPlayerController
 
 local KSysLib = GetKismetSystemLibrary() ---@cast KSysLib UKismetSystemLibrary
 local KMathLib = GetKismetMathLibrary() ---@cast KMathLib UKismetMathLibrary
+local KTextLib = StaticFindObject("/Script/Engine.Default__KismetTextLibrary") ---@cast KTextLib UKismetTextLibrary
+local GameFnLib = StaticFindObject("/Script/FSD.Default__GameFunctionLibrary") ---@cast GameFnLib UGameFunctionLibrary
+
+---@type UFSDGameInstance
+local GameInstance = nil
 
 ---@type boolean
 IsInitialized = false
 
-function Init()
+local function Init()
     if KSysLib == nil then error("KismetSystemLibrary not valid\n") end
     print("[INFO] " .. string.format("KismetSystemLibrary: %s", GetKismetSystemLibrary():GetFullName()))
     if KMathLib == nil then error("KismetMathLibrary not valid\n") end
-    print("[INFO] " .. string.format("KismetMathLibrary: %s", GetKismetMathLibrary():GetFullName()))
-
-    IsInitialized = true
-end
-
-local function DeepDiveInfoMain()
-    Init()
-    if not IsInitialized then
-        error("DeepDiveInfo mod failed to initialize")
-        return
-    end
-
-    ---@type UFSDGameInstance
-    local GameInstance = FindFirstOf("FSDGameInstance")
+    print("[INFO] " .. string.format("KismetMathLibrary: %s", GetKismetMathLibrary():GetFullName() .. "\n"))
+    if KTextLib == nil then error("KismetTexLibrary not valid\n") end
+    print("[INFO] " .. string.format("KTextLib: %s", KTextLib) .. "\n")
+    if GameFnLib == nil then error("GameFunctionLibrary not valid\n") end
+    print("[INFO] " .. string.format("GameFnLib: %s", GameFnLib) .. "\n")
+    GameInstance = FindFirstOf("FSDGameInstance")
     if GameInstance == nil or not GameInstance:IsValid() then
         error("could not find FSDGameInstance")
     end
     print("[INFO] retrieved game instance\n")
 
-    ---@type UDeepDiveManager
-    local DeepDiveManager = GameInstance.DeepDiveManager
-    print("[INFO] retrieved DeepDiveManager: " .. DeepDiveManager:GetFullName() .. "\n")
-
-    local GameFnLib = StaticFindObject('/Script/FSD.Default__GameFunctionLibrary') ---@cast GameFnLib UGameFunctionLibrary
-    local GameplayStatics = UEHelpers.GetGameplayStatics() ---@cast GameplayStatics UGameplayStatics
-    local PlayerController = GameInstance:GetLocalFSDPlayerController()
-    local GameMode = GameFnLib:GetFSDGameMode(PlayerController)
-
-    if GameMode:IsA('/Game/Game/SpaceRig/BP_SpaceRig_GamemOde.BP_SpaceRig_Gamemode_C') then
-        print("[INFO] detected user is loaded into space rig\n")
-        GameMode --[[@as ABP_SpaceRig_Gamemode_C]]:InstantlyStartMission()
-        print("[INFO] attempted to instantly start mission\n")
-    end
+    IsInitialized = true
 end
 
-RegisterKeyBind(Key.X, DeepDiveInfoMain)
+---@param Missions TArray<UGeneratedMission>
+local function PrintDiveMissions(Missions)
+    Missions:ForEach(function(Level, Mission)
+        print(string.format('===STAGE %i: ===', Level))
+        print(string.format('duration: %s', ToString(Mission:get().DurationLimit)))
+        print(string.format('complexity: %s', ToString(Mission:get().ComplexityLimit)))
+        print(string.format('primary obj: %s', ToString(Mission:get().PrimaryObjective)))
+        print(string.format('secondary obj: %s', table.concat(
+            Map(
+                Mission:get().SecondaryObjectives,
+                function(v) return ToString(v) end
+            )
+        ), ', '))
+    end)
+end
+
+local function SelectDeepDive()
+    if not IsInitialized then
+        error("DeepDiveInfo mod failed to initialize")
+        return
+    end
+
+    ---@type UDeepDiveManager
+    local DeepDiveManager = GameInstance.DeepDiveManager
+    ---@type UDeepDive
+    local NormalDeepDive = DeepDiveManager:GetActiveNormalDeepDive()
+    if NormalDeepDive == nil or not NormalDeepDive:IsValid() then
+        error("NormalDeepDive is invalid")
+    end
+    ---@type TArray<UGeneratedMission>
+    local Missions = NormalDeepDive.missions
+
+    PrintDiveMissions(Missions)
+end
+
+RegisterKeyBind(Key.Z, Init)
+RegisterKeyBind(Key.X, SelectDeepDive)
--- DeepDiveInfoMod/Scripts/utils.lua
+++ DeepDiveInfoMod/Scripts/utils.lua
@@ -4,11 +4,22 @@ function ToString(obj)
         return nil
     end
 
-    print(obj:IsValid() and 'valid' or 'invalid')
     local fname = obj:GetFName()
     if fname == nil then
         return obj:GetFullName()
     else
         return fname:ToString()
     end
+end
+
+---@generic K, V, N
+---@param tbl fun(table: {[K]: V}, index?: K):K, V
+---@param f fun(v: V): N
+---@return {[K]: N}
+function Map(tbl, f)
+    local t = {}
+    tbl:ForEach(function(Idx, Elem)
+        t[Idx] = f(Elem:get())
+    end)
+    return t
 end
